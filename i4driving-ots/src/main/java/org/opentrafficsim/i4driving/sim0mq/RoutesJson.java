82c36361aca465f8f399f5895e0eece405c0c07c
Florian Fischer <florian.fischer@vti.se>
2025-10-10 08:53:18 +0200

    Simplify route construction by searching for intermediate nodes

--- i4driving-ots/src/main/java/org/opentrafficsim/i4driving/sim0mq/RoutesJson.java
+++ i4driving-ots/src/main/java/org/opentrafficsim/i4driving/sim0mq/RoutesJson.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.djutils.exceptions.Throw;
 import org.opentrafficsim.core.gtu.GtuType;
+import org.opentrafficsim.core.network.Link;
 import org.opentrafficsim.core.network.NetworkException;
 import org.opentrafficsim.core.network.Node;
 import org.opentrafficsim.core.network.route.Route;
@@ -43,11 +44,26 @@ public class RoutesJson
     {
         for (RouteJson routeJson : this.routes)
         {
-            List<Node> nodes = simulation.getRouteObjectType().getNodes(network, routeJson.objects);
-            Throw.when(nodes == null || nodes.isEmpty(), NetworkException.class,
-                    "Unable to find nodes for route defined by objects %s.", routeJson.objects);
-            Route route = new Route(routeJson.id, gtuType, nodes);
-            network.addRoute(gtuType, route);
+        	Route route = null;
+        	if(routeJson.objects.size() == 2) {
+        		// Simplified route construction by just the first and last link of the route
+        		Link first = network.getLink(routeJson.objects.getFirst());
+        		Node startNode = first.getStartNode();
+        		Link last = network.getLink(routeJson.objects.getLast());
+        		Node endNode = last.getEndNode();
+        		
+        		Route temp = network.getShortestRouteBetween(gtuType, startNode, endNode); 
+                Throw.when(temp == null || temp.size() == 0, NetworkException.class,
+                        "Unable to find nodes for route defined by first and last links: %s.", routeJson.objects);
+                route = new Route(routeJson.id, gtuType,  temp.getNodes());
+        	}
+        	else {
+	            List<Node> nodes = simulation.getRouteObjectType().getNodes(network, routeJson.objects);
+	            Throw.when(nodes == null || nodes.isEmpty(), NetworkException.class,
+	                    "Unable to find nodes for route defined by objects %s.", routeJson.objects);
+	            route = new Route(routeJson.id, gtuType, nodes);
+        	}
+    		network.addRoute(gtuType, route);
         }
     }
 
